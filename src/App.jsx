ae14d50e3f41f905882349a6d8d7b9966c51baa9
konnan933 <95681490+konnan933@users.noreply.github.com>
2024-11-21 15:39:51 +0100

    kezdetleges logo és header eleje

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-main-color'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>Vite + React</h1>
-        </div>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-main-light-color">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='fixed w-full h-full left-0 top-0 flex justify-evenly'>
+      <Header/>
+    </div>
+      
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ function Header() {
 
   return (
     <>
-      <div className='bg-side-color w-full flex items-center justify-center'>
-            bgvsdffsdfsd
+      <div className='bg-side-light-color w-9/12 h-10 flex items-center justify-evenly m-4 rounded-3xl'>
+            <img src="../../public/images/logo.png" width="100px" height="100px"/>
+            <p className="text-side-color">asd</p>
       </div>
     </>
   )
