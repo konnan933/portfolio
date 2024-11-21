f835fdb3d2dc9d7ecda848962392c778f81554bb
konnan933 <95681490+konnan933@users.noreply.github.com>
2024-11-21 13:01:54 +0100

    starting stage

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+
+
+  return (
+    <>
+      <div className='bg-side-color w-full flex items-center justify-center'>
+            bgvsdffsdfsd
+      </div>
+    </>
+  )
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'main-color': '#222831',
+        'side-color': '#31363F',
+        'main-light-color': '#76ABAE',
+        'side-light-color': '#EEEEEE',
+      },
+      backgroung:{
+
+      }
+    },
+  },
+  plugins: [],
+}
